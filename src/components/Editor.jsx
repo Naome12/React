0a16df4b544fe521b19c13bf700f955e90c5e3b9
Naome <tuyishimenaome27@gmail.com>
2023-08-03 09:39:31 +0300

    creating notes

---
+++ src/components/Editor.jsx
@@ -0,0 +1,33 @@
+import Showdown from "showdown"
+import React from "react"
+import ReactMde from "react-mde"
+
+
+ const Editor=({ tempNoteText, setTempNoteText })=>{
+    const [selectedTab, setSelectedTab] = React.useState("write")
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })
+
+    return (
+        <section className="pane editor">
+            <ReactMde
+                value={tempNoteText}
+                onChange={setTempNoteText}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+        </section>
+    )
+}
+
+export default Editor;
